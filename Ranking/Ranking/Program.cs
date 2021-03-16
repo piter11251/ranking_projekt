f60bc0511f598e5918cd9d056c61c8ed18c67862
piter11251 <duszeq13@gmail.com>
2021-03-16 16:29:38 +0100

    implementacja prostego menu

---
+++ Ranking/Ranking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ranking
+{
+    class Program
+    {
+       static void Main()
+        {
+            Menu myMenu = new Menu();
+            myMenu.Menu_ranking();
+        }
+    }
+}
